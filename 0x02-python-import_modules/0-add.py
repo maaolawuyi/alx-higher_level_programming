e3fa488e9e00a1f77cc65f14e9c740082b0e0c3c
mojeed olawuyi <maaolawuyi@gmail.com>
2022-12-01 05:21:32 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0.py import add
-if __name__ = "__main__":
+from add_0 import add
+if __name__ == "__main__":
     a = 1
     b = 2
-
-    print(f"{a} + {b} = {a + b}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
