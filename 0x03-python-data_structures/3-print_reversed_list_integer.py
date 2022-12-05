a39abd5ca68ff3eb7a0cee1de633b0744550afc6
mojeed olawuyi <maaolawuyi@gmail.com>
2022-12-05 06:22:46 -0800

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    n = len(my_list)
+    if my_list is None:
+        return None
+    else:
+        reversed = my_list[:: -}
+        for i in range(n)
+        print("{:d}".format(reversed[i]))
