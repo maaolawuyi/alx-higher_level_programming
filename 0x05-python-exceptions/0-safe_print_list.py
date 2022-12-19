813aee3144f8ab6238d111dbd09ffa963e0ed688
mojeed olawuyi <maaolawuyi@gmail.com>
2022-12-19 08:19:55 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(i), end="")
             count += 1
-        except Exception:
+        except IndexError:
             print()
     print()
     return count
