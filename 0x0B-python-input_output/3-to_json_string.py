3b7433cd00e67d4619727ac0d55874c16f90b13b
mojeed olawuyi <maaolawuyi@gmail.com>
2023-01-10 08:38:42 -0800

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+import json
+
+
+def to_json_string(my_obj):
+    """ to_json_strin
+    Args:
+       my_obj (dict): dictionary
+
+    Returns:
+       a JSON
+
+    """
+
+    return json.dumps(my_obj)
+
 
