0a743924a6b35c0868c20bdc078ac4a70f5e6b0a
mojeed olawuyi <maaolawuyi@gmail.com>
2023-01-10 08:14:19 -0800

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads n lines of a text file
-"""
-
 def read_lines(filename="", nb_lines=0):
-    """ Function that reads from a file and prints its number of lines
-
+    """ read_lines
     Args:
-        filename: filename
-        nb_lines: number of lines to print
+        filename (path): file path
+        nb_lines (int): number of lines
 
-    Raises
-        Exception: when the file can be opened
-
+    Returns:
+        read the lines a print to stdout
     """
-
-    with open(filename, 'r', encoding="utf-8") as file:
+    with open(filename, encoding='utf-8') as file:
         if nb_lines <= 0:
             print(file.read(), end="")
         else:
             for i in range(nb_lines):
                 print(file.readline(), end="")
-
